b017911c6636e8dd0727c81d516b71df5d806c98
rizonegustama <rizonegustama@gmail.com>
2020-09-26 15:34:02 +0700

    upload

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+document.getElementById("jNusa").innerHTML = "Nusa Putra University";
+document.getElementById("isiSejarah").innerHTML = "Universitas Nusa Putra berdiri berdasarkan Keputusan Menteri Riset, Teknologi dan Pendidikan Tinggi Nomor : 108/KPT/I/2018 Tentang Perubahan Bentuk Sekolah Tinggi Teknologi Nusa Putra di Sukabumi yang dikelola oleh Yayasan Perguruan Nusa Putra Sukabumi menjadi Universitas Nusa Putra di Sukabumi yang diselenggarakan oleh Yayasan Tepatnya pada hari Jumat tanggal 02 Februari 2018, sejak hari itulah secara resmi Universitas Nusa Putra yang sering di singkat NPU (Nusa Putra University) secara resmi berdiri.";
+document.getElementById("isiGagasan").innerHTML = "Menjadikan sikap pengasih dan penyayang sebagai pijakan nurani kesempurnaan ahlak generasi baru Nusa Putra dan prilaku cinta kasih sebagai ketundukan kepada Illahiyah. <br> Organisasi digunakan sebagai wahana strategis menanamkan sikap Cinta Kasih Illahiyah, Cinta kasih Orang Tua dan Cinta Kasih Sesama Manusia dilandasi atas sikap Pengasih dan Penyayang yang melekat pada nurani kebenaran Generasi Nusa Putra dalam upaya menjaga kesempurnaan ahlak sebagai manusia.";
+
+document.getElementById("isiVisi").innerHTML = 
+        "<ol>" +
+            "<li>Memajukan ilmu pengetahuan dan teknologi melalui pendidikan, pengajaran dan pembelajaran yang unggul dan bermutu tinggi</li>" +
+            "<li>Memajukan ilmu pengetahuan dan teknologi, melalui pengkajian, penelitian, publikasi dan pematenan hasil penelitian yang unggul dan bermutu tinggi.</li>" +
+            "<li>Memajukan ilmu pengetahuan dan teknologi, yang dapat menjawab masalah dan tantangan masyarakat yang unggul dan bermutu tinggi.</li>" +
+            "<li>Mengembangkan organisasi yang tangguh dan inovatif dengan memanfaatkan potensi sumberdaya secara optimal sehingga dapat adil dalam mencerdaskan kehidupan bangsa dan Negara.</li>" +
+            "<li>Mendorong tumbuhnya kemampuan penguasaan ilmu dan teknologi di Indonesia demi terciptanya kemandirian bangsa.</li>" +
+        "</ol>"
+
+document.getElementById("isiVisi1").innerHTML = "<i>Menjadi institusi pendidikan tinggi unggul melampaui standar nasional pada tahun 2030 dan kelas dunia pada tahun 2045 di bidang sosial, sains dan teknologi yang tangguh sebagai ujung tombak untuk menghasilkan Sumber Daya Manusia yang cerdas, kreatif, inovatif dan religius</i>";
+
+document.getElementById("aboutme").innerHTML = "<h3>Rizwan Gustama</h3>" +
+"<p>" +
+    "rizonegustama@gmail.com <br>" +
+    "Nusa Putra University<br>" +
+    "<b>Teknik Informatika</b>" +
+"</p>"
